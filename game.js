4f6c95b7efbc4faf57e193dfba1fb51dc5c0bb12
SamBaksht <sam@sambaksht.com>
2025-10-19 19:26:27 -0500

    Brick Class

--- game.js
+++ game.js
@@ -8,23 +8,70 @@ const ctx = gameCanvas.getContext("2d");
 class Game {
     constructor() {
         this.score = 0;
+        this.paddle = new Paddle();
 
+        this.bricks = [];
+        for(let layer = 4; layer >= 0; layer--) {
+            for(let gridNumber = 0; gridNumber < 10; gridNumber++) {
+                this.bricks.push(new Brick(layer, gridNumber))
+            }
+        }
+        window.addEventListener("keydown", (event) => {
+            if (event.key === "ArrowLeft" && this.paddle.xPosition > 0.1) {
+                this.paddle.xPosition -= 0.05;
+            } else if (event.key === "ArrowRight" && this.paddle.xPosition < 0.95) {
+                this.paddle.xPosition += 0.05;
+            }
+        });
+    }
+
+    update() {
+        gameCanvas.width = window.innerWidth;
+        gameCanvas.height = window.innerHeight;
+
+        ctx.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+        this.paddle.update()
+        for(let brick of this.bricks) {
+            brick.draw()
+        }
     }
 }
 
 class Ball {
-
-}
- 
-class Brick {
-static colors = ["#0000FF", "#3333FF", "#6666FF", "#9999FF", "#FF0000"]
-    constructor(hitPoints, xPosition, yPosition) {
-        this.color = Brick.colors[hitPoints];
+    constructor() {
         
     }
 }
+ 
+class Brick {
+static colors = ["#FF0000", "#9999FF", "#6666FF", "#3333FF", "#0000FF"];
+    constructor(hitPoints, gridNumber) {
+        this.color = Brick.colors[hitPoints];
+        this.hitPoints = hitPoints;
+        this.gridNumber = gridNumber;
 
-class bottomMoveThing {
+    }
+
+    draw() {
+        const xPixelPosition = this.gridNumber * (gameCanvas.width / 10)
+        const yPixelPosition = this.hitPoints * (gameCanvas.height / 10)
+        const brickHeight = gameCanvas.height * 0.05;
+        const brickWidth = gameCanvas.width * 0.08
+
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.fillRect(
+            xPixelPosition,
+            yPixelPosition,
+            brickWidth,
+            brickHeight
+        );
+        ctx.closePath();
+
+    }
+}
+
+class Paddle {
     constructor() {
         this.xPosition = 0.5; // Center Horizontally
         this.color = "blue";
@@ -35,9 +82,9 @@ class bottomMoveThing {
     }
 
     draw() {
-        const bottomMoveThingWidth = gameCanvas.width * 0.1;
-        const bottomMoveThingHeight = gameCanvas.height * 0.04;
-        const xPixelPosition = (this.xPosition * gameCanvas.width) - (bottomMoveThingWidth / 2);
+        const paddleWidth = gameCanvas.width * 0.1;
+        const paddleHeight = gameCanvas.height * 0.04;
+        const xPixelPosition = (this.xPosition * gameCanvas.width) - (paddleWidth / 2);
         const yPixelPosition = gameCanvas.height * 0.95;
         
         ctx.beginPath();
@@ -45,17 +92,18 @@ class bottomMoveThing {
         ctx.fillRect(
             xPixelPosition,
             yPixelPosition,
-            bottomMoveThingWidth,
-            bottomMoveThingHeight
+            paddleWidth,
+            paddleHeight
         );
-
+        ctx.closePath();
     }
 }
 
 
-const thing = new bottomMoveThing() 
-thing.update()
+const game = new Game();
 
-
+function loop() {
+    game.update();
+}
 
 setInterval(() => loop(), 1000 / 60) // 60 FPS
